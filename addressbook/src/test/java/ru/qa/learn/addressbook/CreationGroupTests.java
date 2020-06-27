8d2b49273612d8d4002ac878748f95478e8a19bc
RYuriy2 <iun.revin@severstal.com>
2020-06-27 16:17:33 +0300

    модификация метода инициализации и завершения вебдрайвера

--- addressbook/src/test/java/ru/qa/learn/addressbook/CreationGroupTests.java
+++ addressbook/src/test/java/ru/qa/learn/addressbook/CreationGroupTests.java
@@ -17,10 +17,6 @@ public class CreationGroupTests {
     wd = new ChromeDriver();
     baseUrl = "https://www.google.com/";
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testCreationGroup() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -28,6 +24,10 @@ public class CreationGroupTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.id("LoginForm")).submit();
+  }
+
+  @Test
+  public void testCreationGroup() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -41,11 +41,11 @@ public class CreationGroupTests {
     wd.findElement(By.name("group_footer")).sendKeys("TestComment1");
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
+    wd.findElement(By.linkText("Logout")).click();
     wd.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
