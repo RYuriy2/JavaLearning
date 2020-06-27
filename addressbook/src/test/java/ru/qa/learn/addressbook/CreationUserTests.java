8b150b3df3d6e42947fc83fb147366a186222b94
RYuriy2 <iun.revin@severstal.com>
2020-06-27 20:39:51 +0300

    тест создания нового пользователя

---
+++ addressbook/src/test/java/ru/qa/learn/addressbook/CreationUserTests.java
@@ -0,0 +1,111 @@
+package ru.qa.learn.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+
+public class CreationUserTests {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+  @Test
+  public void testCreationUserTests() throws Exception {
+    initCreationNewUser();
+    fillUserData(new UserData("Михаил", "Михайлович", "Буслаев", "+79009009090", "test@test.com"));
+    submitCreateNewUser();
+    returnToHomePage();
+  }
+
+  private void returnToHomePage() {
+    driver.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitCreateNewUser() {
+    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillUserData(UserData userData) {
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
+    driver.findElement(By.name("middlename")).clear();
+    driver.findElement(By.name("middlename")).sendKeys(userData.getMidlname());
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys(userData.getLastname());
+    driver.findElement(By.name("mobile")).clear();
+    driver.findElement(By.name("mobile")).sendKeys(userData.getPhoneNumber());
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys(userData.getEmail());
+      }
+
+  private void initCreationNewUser() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+
+  private void login(String username, String password) {
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.id("LoginForm")).submit();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    logout();
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private void logout() {
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ addressbook/src/test/java/ru/qa/learn/addressbook/UserData.java
@@ -0,0 +1,37 @@
+package ru.qa.learn.addressbook;
+
+public class UserData {
+    private final String firstname;
+    private final String midlname;
+    private final String lastname;
+    private final String phoneNumber;
+    private final String email;
+
+    public UserData(String firstname, String midlname, String lastname, String phoneNumber, String email) {
+        this.firstname = firstname;
+        this.midlname = midlname;
+        this.lastname = lastname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMidlname() {
+        return midlname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
