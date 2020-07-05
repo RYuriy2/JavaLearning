5ed4676d7d5112f7d474526c3d444e1826b3e9a5
RYuriy2 <iun.revin@severstal.com>
2020-07-05 13:55:39 +0300

    create HelperBaseClass

--- addressbook/src/test/java/ru/qa/learn/addressbook/appmanager/UserHelper.java
+++ addressbook/src/test/java/ru/qa/learn/addressbook/appmanager/UserHelper.java
@@ -4,43 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.qa.learn.addressbook.model.UserData;
 
-public class UserHelper {
-    private WebDriver wd;
+public class UserHelper extends HelperBase {
 
     public UserHelper(WebDriver wd) {
-        this.wd=wd;
+        super(wd);
     }
 
     public void selectUser() {
-        wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
 
     public void returnToHomePage() {
-      wd.findElement(By.linkText("home page")).click();
+        click(By.linkText("home page"));
     }
 
     public void submitCreateNewUser() {
-      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+        click(By.xpath("(//input[@name='submit'])[2]"));
     }
 
     public void fillUserData(UserData userData) {
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
-      wd.findElement(By.name("middlename")).clear();
-      wd.findElement(By.name("middlename")).sendKeys(userData.getMidlname());
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(userData.getLastname());
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(userData.getPhoneNumber());
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(userData.getEmail());
-        }
+        type(By.name("firstname"),userData.getFirstname());
+        type(By.name("middlename"),userData.getMidlname());
+        type(By.name("lastname"),userData.getLastname());
+        type(By.name("mobile"),userData.getPhoneNumber());
+        type(By.name("email"),userData.getEmail());
+    }
 
     public void initCreationNewUser() {
-      wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 
     public void deleteSelectedUser() {
-      wd.findElement(By.xpath("//input[@value='Delete']")).click();
+        click(By.xpath("//input[@value='Delete']"));
     }
 }
