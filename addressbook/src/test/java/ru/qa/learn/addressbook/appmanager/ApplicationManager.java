3bf322084c9bf162dcb464aa05f372aada8c4a34
RYuriy2 <iun.revin@severstal.com>
2020-07-12 14:03:17 +0300

    multibrowser tests

--- addressbook/src/test/java/ru/qa/learn/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/qa/learn/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,10 @@ package ru.qa.learn.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,16 +15,30 @@ public class ApplicationManager {
     private UserHelper userHelper;
     private GroupHelper groupHelper;
     WebDriver wd;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        } else if (browser == BrowserType.EDGE) {
+            wd = new EdgeDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/group.php");
+        wd.get("http://localhost/addressbook");
         groupHelper = new GroupHelper(wd);
         userHelper = new UserHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin","secret");
+        sessionHelper.login("admin", "secret");
     }
 
     public void stop() {
@@ -29,7 +47,7 @@ public class ApplicationManager {
     }
 
     public void closeAlertPopUp() {
-      wd.switchTo().alert().accept();
+        wd.switchTo().alert().accept();
     }
 
     public GroupHelper getGroupHelper() {
