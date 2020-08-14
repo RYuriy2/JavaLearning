8099691740f3450c2563aa1907b75125429389c1
RYuriy2 <iun.revin@severstal.com>
2020-08-14 21:11:44 +0300

    lection 8.5 upload and restore config file

--- mantistests/src/test/java/ru/mantis/appmanager/ApplicationManager.java
+++ mantistests/src/test/java/ru/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
     private WebDriver wd;
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser)  {
         this.browser = browser;
@@ -66,5 +67,12 @@ public class ApplicationManager {
             wd.get(properties.getProperty("web.baseURL"));
         }
         return wd;
+    }
+
+    public FtpHelper ftp(){
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
     }
 }
