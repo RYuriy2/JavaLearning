b080d367a421bbcc9f532fab7280182357495744
RYuriy2 <iun.revin@severstal.com>
2020-08-14 21:53:29 +0300

    fix task 16

--- addressbook/src/test/java/ru/qa/learn/addressbook/tests/UserAdditionToGroupTests.java
+++ addressbook/src/test/java/ru/qa/learn/addressbook/tests/UserAdditionToGroupTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.qa.learn.addressbook.model.GroupData;
 import ru.qa.learn.addressbook.model.Groups;
 import ru.qa.learn.addressbook.model.UserData;
+import ru.qa.learn.addressbook.model.Users;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,9 +14,31 @@ public class UserAdditionToGroupTests extends TestBase {
 
     @BeforeMethod
     public void ensurePrecinditions() {
-        if (app.db().groups().size() == 0) {                            //Проверяем количество существующих групп
-            app.goTo().groupPage();                                     //Если нет ни одной группы то переходим на страницу групп
-            app.group().create(new GroupData().withName("testnew"));    //И добавляем новую группу с заданным именем
+        Users users = app.db().users();                                         //Получаем список существующих пользователей
+        if (app.db().groups().size() == 0) {                                    //Проверяем количество существующих групп
+            app.goTo().groupPage();                                             //Если нет ни одной группы то переходим на страницу групп
+            app.group().create(new GroupData().withName("testnew"));            //И добавляем новую группу с заданным именем
+        }
+        if (users.size() == 0) {                                                //Проверяем количество пользователей
+            app.goTo().homePage();                                              //Если нет ни одного пользователя, то переходим на домашнюю страницу
+            app.user().create(new UserData()                                    //И создаём нового пользователя с заданными параметрами
+                            .withLastname("Михаил")                             //Имя
+                            .withAddress("Михайлович")                          //Адресс
+                            .withFirstname("Буслаев")                           //Фамилия
+                            .withHomePhoneNumber("+79009009090")                //Домашний телефон
+                            .withEmail1("test@test.com")                        //E-mail
+                            .inGroup(app.db().groups().iterator().next())       //Группа
+                    , true);
+        } else {                                                                //Если есть хотя бы один пользователь
+            for (UserData user : users){                                        //То по всему списку пользователей
+                if (user.getGroups().size() == app.db().groups().size()){       //Проверяем каждого пользователя, состоит ли он во всех группах
+                    users.remove(user);                                         //И удаляем из списка тех, кто состоит во всех
+                }
+            }
+            if (users.size() == 0){                                             //Если в итоге список становится пустым
+                app.goTo().groupPage();                                         //То переходим на страницу групп
+                app.group().create(new GroupData().withName("testnew3"));       //И добавляем новую группу с заданным именем
+            }
         }
     }
 
@@ -23,8 +46,13 @@ public class UserAdditionToGroupTests extends TestBase {
     public void additionToGroupTest(){
         app.goTo().homePage();                                                                      //Переходим на домашнюю страницу для дальнейшей работы с данными на ней
         Groups groups = app.db().groups();                                                          //Считываем список существующих групп
-        UserData user = app.db().users().iterator().next();                                         //Выбираем случайного пользователя для добавления в групу
-
+        Users users = app.db().users();                                                             //Получаем список пользователей
+        for (UserData user : users){                                                                //По всему списку пользователей
+            if (user.getGroups().size() == app.db().groups().size()){                               //Проверяем, состоит ли пользователь во всех группах
+                users.remove(user);                                                                 //И удаляем из списка тех, кто состоит во всех
+            }
+        }
+        UserData user = users.iterator().next();                                                    //Из оставшегося списка пользователей берём случайного для дальнейшей работы с ним
         if (user.getGroups().size() != groups.size()){                                              //Сравниваем количество существующих групп с количеством групп пользователя
             GroupData additionGroup = app.user().groupsWithoutUser(user,groups).iterator().next();  //Если количество не совпадает, то ищем все группы, в которых пользователь не состоит и берём случайную
 
--- addressbook/src/test/java/ru/qa/learn/addressbook/tests/UserDelitionFromGroupTests.java
+++ addressbook/src/test/java/ru/qa/learn/addressbook/tests/UserDelitionFromGroupTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.qa.learn.addressbook.model.GroupData;
 import ru.qa.learn.addressbook.model.Groups;
 import ru.qa.learn.addressbook.model.UserData;
+import ru.qa.learn.addressbook.model.Users;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,9 +13,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class UserDelitionFromGroupTests extends TestBase {
     @BeforeMethod
     public void ensurePrecinditions() {
+        Users users = app.db().users();                                 //Получаем список всех существующих пользователей
         if (app.db().groups().size() == 0) {                            //Проверяем количество существующих групп
             app.goTo().groupPage();                                     //Если нет ни одной группы то переходим на страницу групп
-            app.group().create(new GroupData().withName("testnew"));    //И добавляем новую группу с заданным именем
+            app.group().create(new GroupData().withName("testnew1"));   //И добавляем новую группу с заданным именем
         }
         if (app.db().users().size() == 0) {                             //Проверяем количество пользователей
             app.goTo().homePage();                                      //Если нет ни одного пользователя. то переходим на домашнюю страницу
@@ -26,13 +28,29 @@ public class UserDelitionFromGroupTests extends TestBase {
                     .withEmail1("test@test.com")                        //E-mail
                     .inGroup(app.db().groups().iterator().next())       //Группа
                     , true);
+        } else {                                                        //Если есть хотя бы один пользователь
+            for (UserData user : users){                                //То по всему списку пользователей
+                if (user.getGroups().size() == 0){                      //Проверяем, состоит ли он хотя бы в одной группе
+                    users.remove(user);                                 //Удаляем из списка пользователей тех, кто не состоит ни в одной группе
+                }
+            }
+            if (users.size() == 0){                                     //Если в итоге ни один пользователь не состоит ни в одной группе
+                app.user().addToGroup(app.db().users().iterator().next()//Добавляем случайного пользователя
+                        ,app.db().groups().iterator().next());          //В случайную группу
+            }
         }
     }
 
     @Test
     public void userDelitionFromGroupTest(){
         app.goTo().homePage();                                                  //Переходим на домашнюю страницу для дальнейшей работы
-        UserData user = app.db().users().iterator().next();                     //Выбираем пользователя, с которым будем работать
+        Users users = app.db().users();                                         //Выбираем пользователя, с которым будем работать
+        for (UserData user : users){                                            //То по всему списку пользователей
+            if (user.getGroups().size() == 0){                                  //Проверяем, состоит ли он хотя бы в одной группе
+                users.remove(user);                                             //Удаляем из списка пользователей тех, кто не состоит ни в одной группе
+            }
+        }
+        UserData user = users.iterator().next();                                //Из оставшегося списка берём случайного пользователя
 
         if (user.getGroups().size() != 0){                                      //Проверяем количество групп, в которых присутствует пользователь
             Groups before = user.getGroups();                                   //Если количество больше 0, то сохраняем список групп пользователя до модицикации
